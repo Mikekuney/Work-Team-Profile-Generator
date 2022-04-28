6f98bb7009f6cb7b07bdaa53fee3ef3f7ef8e66f
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-04-28 08:43:12 -0400

    added engineer.test.js

---
+++ tests/engineer.test.js
@@ -0,0 +1,14 @@
+const Engineer = require("../lib/assets/js/engineer");
+
+test("creates a new engineer", () => {
+    const engineer = new Engineer("Richard");
+
+    engineer.id = 1111;
+    engineer.email = "richard@mail.com";
+    engineer.github = "richard2";
+
+    expect(engineer.name).toBe("Richard");
+    expect(engineer.id).toEqual(expect.any(number));
+    expect(engineer.email).toBe("richard@mail.com");
+    expect(engineer.github).toBe("richard2");
+});
